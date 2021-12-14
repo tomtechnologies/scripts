17b8e7591d41a20359f2fe41fcf2689d24324de4
tomtechnologies <tomtechnologies@users.noreply.github.com>
2021-12-14 21:01:57 +1100

    Update docktest.sh

--- docktest.sh
+++ docktest.sh
@@ -1,7 +1,7 @@
-list="https://github.com/nextcloud/docker https://github.com/phpmyadmin/docker https://github.com/deviantony/docker-elk https://github.com/nginx-proxy/nginx-proxy https://github.com/sameersbn/docker-gitlab https://github.com/creack/docker-firefox"
+list="github.com/nextcloud/docker github.com/phpmyadmin/docker github.com/deviantony/docker-elk github.com/nginx-proxy/nginx-proxy github.com/sameersbn/docker-gitlab github.com/creack/docker-firefox"
 
-
+IFS=" "
 for proj in $list
 do
- docker build $proj
+ time docker build $proj
 done
