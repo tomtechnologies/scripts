16e49a81019e2127c43122dcf47c8b5e4299c0f0
tomtechnologies <tomtechnologies@users.noreply.github.com>
2021-12-15 10:39:01 +1100

    Update docktest.sh

--- docktest.sh
+++ docktest.sh
@@ -1,7 +1,19 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+
 list="github.com/nextcloud/docker github.com/phpmyadmin/docker github.com/deviantony/docker-elk github.com/nginx-proxy/nginx-proxy github.com/sameersbn/docker-gitlab github.com/creack/docker-firefox"
+
+images=$(docker image ls | grep -v REPOS | awk '{print $3}')
+
+IFS='\n'
+for image in $images
+do
+ docker image rm $image # Will remove any existing images
+done
+
 
 IFS=" "
 for proj in $list
 do
- time docker build $proj
+ time docker build $proj --no-cache
 done
